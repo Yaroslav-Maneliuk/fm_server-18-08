cac78c5fc80c2485a4ab2e441461a49bf39f539c
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-08-18 12:06:43 +0300

    node

---
+++ index.js
@@ -0,0 +1,19 @@
+const otherObject = require("./utils/other");
+
+console.log(otherObject)
+
+
+/*
+resolving -> loading -> wrapping -> evalution -> caching
+
+1)
+  core modules
+  node modules
+2)
+  2.1  fileName. js | json
+  2.2  dirName
+          package.json  main: example.js
+          index.  js | json
+3)
+  ERROR
+ */
---
+++ utils/other.js
@@ -0,0 +1,8 @@
+module.exports.test = "test";
+const test2 = "test2";
+
+module.exports.thing = { name: "mobile", model: "sony" };
+const thing2 = "thing2";
+
+module.exports.f1 = () => "f1";
+const f2 = "f2";

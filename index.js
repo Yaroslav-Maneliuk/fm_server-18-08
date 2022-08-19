3b8ba25be7210c6a79076d1e64b068fb274ec334
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-08-19 12:02:21 +0300

    Added spaghetti code on the server

--- index.js
+++ index.js
@@ -1,19 +1,66 @@
-const otherObject = require("./utils/other");
+const http = require("http");
+const fs = require("fs/promises");
+// const fs = require("fs").promises;
+const PORT = 3000;
+const users = [];
 
-console.log(otherObject)
+const routerGET = {
+  "/": async (req, res) => {
+    const data = await fs.readFile("./views/index.html", "utf8");
+    return res.end(data);
+  },
+  "/about.html": async (req, res) => {
+    const data = await fs.readFile("./views/about.html", "utf8");
+    return res.end(data);
+  },
+  "/contact.html": async (req, res) => {
+    const data = await fs.readFile("./views/contact.html", "utf8");
+    return res.end(data);
+  },
+};
 
+const requestListener = async (req, res) => {
+  const { method, url } = req;
+  if (method === "GET" && routerGET[url]) {
+    return routerGET[url](req, res);
+  }
+  // if (method === "GET") {
+  //   if (url === "/") {
+  //     const data = await fs.readFile("./views/index.html", "utf8");
+  //     return res.end(data);
+  //   }
+  //   if (url === "/about.html") {
+  //     const data = await fs.readFile("./views/about.html", "utf8");
+  //     return res.end(data);
+  //   }
+  //   if (url === "/contact.html") {
+  //     const data = await fs.readFile("./views/contact.html", "utf8");
+  //     return res.end(data);
+  //   }
+  // }
+  if (method === "POST") {
+    if (url === "/create-user") {
+      let jsonString = "";
+      req.on("data", (chunk) => {
+        jsonString += chunk;
+      });
+      req.on("end", () => {
+        const user = JSON.parse(jsonString);
+        user.id = Date.now();
+        users.push(user);
+        console.log(users);
+        res.end(JSON.stringify(user));
+      });
+      return;
+    }
+  }
+  fs.readFile("./views/404.html", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.end(data);
+  });
+};
 
-/*
-resolving -> loading -> wrapping -> evalution -> caching
-
-1)
-  core modules
-  node modules
-2)
-  2.1  fileName. js | json
-  2.2  dirName
-          package.json  main: example.js
-          index.  js | json
-3)
-  ERROR
- */
+const server = http.createServer(requestListener);
+server.listen(PORT);
--- utils/other.js
+++
@@ -1,8 +0,0 @@
-module.exports.test = "test";
-const test2 = "test2";
-
-module.exports.thing = { name: "mobile", model: "sony" };
-const thing2 = "thing2";
-
-module.exports.f1 = () => "f1";
-const f2 = "f2";
